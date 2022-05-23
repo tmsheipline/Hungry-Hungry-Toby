b78de5ce9bff2c320ca703f5bfec38dac538b56f
tmsheipline <taylorsheipline_2012@hotmail.com>
2022-05-23 14:42:34 -0400

    Board setup changed, No bottom play area of grid - bigger game space ideally.

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 // Initial setting of the canvas and context
-let ctx = document.getElementById('game');
+let canvas = document.getElementById('game');
+let ctx = canvas.getContext('2d')
+
+canvas.setAttribute("width", getComputedStyle(canvas)["width"])
+canvas.setAttribute("height", getComputedStyle(canvas)["height"])
