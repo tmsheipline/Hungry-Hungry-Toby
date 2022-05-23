42704b89b9fe475fbd3821d1cba095c5ed6c4a65
tmsheipline <taylorsheipline_2012@hotmail.com>
2022-05-23 16:08:22 -0400

    Canvas setup complete and dimensions of canvas with DOM selection

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-// Initial setting of the canvas and context
+// ====================== Setting Canvas and Context ====================//
 let canvas = document.getElementById('game');
 let ctx = canvas.getContext('2d')
 
+// ====================== PAINT INTIAL SCREEN =========================== //
 canvas.setAttribute("width", getComputedStyle(canvas)["width"])
 canvas.setAttribute("height", getComputedStyle(canvas)["height"])
+
+// ====================== Variables / DOM Manipulation ==================//
