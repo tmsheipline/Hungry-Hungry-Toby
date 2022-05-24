017376ff8c33d50b5e330cbc703d83e0a1d6d7d1
tmsheipline <taylorsheipline_2012@hotmail.com>
2022-05-24 13:09:58 -0400

    All pictures loaded into HTML with display none

--- app.js
+++ app.js
@@ -1,31 +1,27 @@
 // ====================== Setting Canvas and Context ====================//
 let canvas = document.getElementById('game');
 let ctx = canvas.getContext('2d')
-let image = document.querySelector('#tobypic');
 
 // ====================== PAINT INTIAL SCREEN =========================== //
 canvas.setAttribute("width", getComputedStyle(canvas)["width"])
 canvas.setAttribute("height", getComputedStyle(canvas)["height"])
 
 // ====================== Variables / DOM Manipulation ==================//
-let hamburger;
-let rubberband;
-// let toby;
-let yarn;
+let tobyImage = document.querySelector('#tobypic');
+let rubberbandImage = document.querySelector('#')
+let yarnImage = document.querySelector('#')
+let chickenImage = document.querySelector('#')
+let hamburgerImage = document.querySelector('#')
+
 
 //========== Images to load =================//
 
-let hamburgerImage = new Image();
-hamburgerImage.src = "img/hamburger icon.png"
 
-let chickenImage = new Image();
-chickenImage.src = "img/chickenlegicon.png"
 
-let yarnImage = new Image();
-yarnImage.src = "img/yarniconreal.png"
 
-let rubberbandImage = new Image();
-rubberbandImage.src = "img/rubberband icon.png"
+
+
+
 // =================== Function or class for the main character (Toby)===================//
 class CharacterMaker {
     constructor (x, y, width, height, speed, image){
@@ -47,7 +43,7 @@ class CharacterMaker {
     (function (){
         // let tobyImage = new Image();
         // tobyImage.src = "img/frontviewtoby.png"
-        toby = new CharacterMaker(0, 0, 100, 200, 2, image);
+        toby = new CharacterMaker(0, 0, 100, 200, 2, tobyImage);
         console.log(toby);
         toby.render();
         // console.log(tobyImage);
