90f9433d5d0609ffe380042164a984a3dde2c3d7
tmsheipline <taylorsheipline_2012@hotmail.com>
2022-05-24 12:01:25 -0400

    Functioning DOM content loaded, event listener not compatible with browser

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // ====================== Setting Canvas and Context ====================//
 let canvas = document.getElementById('game');
 let ctx = canvas.getContext('2d')
+let image = document.querySelector('#tobypic');
 
 // ====================== PAINT INTIAL SCREEN =========================== //
 canvas.setAttribute("width", getComputedStyle(canvas)["width"])
@@ -8,11 +9,11 @@ canvas.setAttribute("height", getComputedStyle(canvas)["height"])
 
 // ====================== Variables / DOM Manipulation ==================//
 let hamburger;
-let hairtie;
+let rubberband;
+// let toby;
+let yarn;
 
 //========== Images to load =================//
-let tobyImage = new Image();
-tobyImage.src = "img/front view toby.png"
 
 let hamburgerImage = new Image();
 hamburgerImage.src = "img/hamburger icon.png"
@@ -26,44 +27,65 @@ yarnImage.src = "img/yarniconreal.png"
 let rubberbandImage = new Image();
 rubberbandImage.src = "img/rubberband icon.png"
 // =================== Function or class for the main character (Toby)===================//
-class TobyMaker {
-    constructor (url, x, y, width, height){
-    this.url = url;
-    this.x = x;
-    this.y = y;
-    this.height = height;
-    this.width = width;
-    this.alive = true;
-    this.render = function (){
-        ctx.drawImage(this.url, this.x, this.y, this.width, this.height);
-    };
-}};
+class CharacterMaker {
+    constructor (x, y, width, height, speed, image){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+        this.image = image;
+        this.alive = true;
+        this.render = function (){
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        }
+    }};
+    
+    
+    
+    // === Event Listener to get Toby on the gameboard ==========//
+    (function (){
+        // let tobyImage = new Image();
+        // tobyImage.src = "img/frontviewtoby.png"
+        toby = new CharacterMaker(0, 0, 100, 200, 2, image);
+        console.log(toby);
+        toby.render();
+        // console.log(tobyImage);
+        // shrek = new Crawler(100, 200, "#bada55", 40, 80);
+    
+        // console.log(donkey, shrek);
+    })()
+    
+    // window.addEventListener('DOMContentLoaded', function (e){
+    //     toby = new CharacterMaker(0, 0, 100, 200, 2, image);
+    //     console.log(toby);
+    //     toby.render();
+    // });
+    // toby = new CharacterMaker(20, 30, 100, 200, 2, tobyImage);
+    // console.log(toby);
+// // ========== KEYBOARD INTERACTION LOGIC ============= //
+// function keyboardMovement(e){
+//     console.log('the key that was presssed was' + e.key);
 
-let toby = new TobyMaker("img/front view toby.png", 200, 400, 20, 40);
-console.log(toby);
+//     switch (e.key){
+//         case "ArrowLeft":
+//             toby.x > 0 ? toby.x -= 10 : null;
+//             break
+//         case "ArrowRight":
+//             toby.x < (game.width - toby.width) ? toby.x += 10 : null;
+
+//     }
+// // console.log(toby);
+// }
+
+// ======== Game Loop Logic ============//
+function gameloop(){
+
+    //render game characters
 
 
 
 
-// === Event Listener to get Toby on the gameboard ==========//
-window.addEventListener('DOMContentLoaded', function (e){
-    toby = new TobyMaker(tobyImage, 100, 20, 40, 50);
-})
-
-
-// ========== KEYBOARD INTERACTION LOGIC ============= //
-function keyboardMovement(e){
-    console.log('the key that was presssed was' + e.key);
-
-    switch (e.key){
-        case "ArrowLeft":
-            toby.x > 0 ? toby.x -= 10 : null;
-            break
-        case "ArrowRight":
-            toby.x < (game.width - toby.width) ? toby.x += 10 : null;
-
-    }
-// console.log(toby);
 
 
 }
