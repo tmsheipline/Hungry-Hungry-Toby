ac1dc309f7d566743eabd775e2bf3618b9818969
tmsheipline <taylorsheipline_2012@hotmail.com>
2022-05-24 17:01:58 -0400

    Create another constructor for falling objects

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let score = document.querySelector('#score')
 let lives = document.querySelector('#lifecount')
 let hamburger;
 let toby;
+let yarn;
 
 // ====================== Setting Canvas and Context =========================== //
 canvas.setAttribute("width", getComputedStyle(canvas)["width"])
@@ -17,7 +18,7 @@ let yarnImage = document.querySelector('#yarnpic')
 let chickenImage = document.querySelector('#chickenpic')
 let hamburgerImage = document.querySelector('#burgerpic')
 
-// =================== Class Constructor for the character objects===================//
+// =================== Class Constructor for the main character object================== //
 class CharacterMaker {
     constructor (x, y, width, height, speed, image){
         this.x = x;
@@ -30,14 +31,35 @@ class CharacterMaker {
         this.render = function (){
             ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
         }
-    }};
+    }
+};
+
+// ============= Class Constructor for all falling objects =================== //
+class FallingObject {
+    constructor (x, y, width, height, speed, image){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = width;
+        this.speed = speed;
+        this.image = image;
+        this.alive = true;
+        this.render = function (){
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        }
+    }
+};
+
+// ======= class constructor for the falling items =============//
+ 
     
     // === Event Listener to get images on the gameboard ==========//
     (function (){
         toby = new CharacterMaker(300, 280, 150, 250, 1000, tobyImage);
         toby.render();
-        console.log(toby);
+        // console.log(toby);
 
+// ========= Don't want falling objects directly loaded onto screen on load =========//
         // hamburger = new CharacterMaker(0, 0, 50, 50, 3, hamburgerImage);
         // hamburger.render();
 
@@ -49,6 +71,8 @@ class CharacterMaker {
 
         // rubberband = new CharacterMaker(730, 0, 50, 50, 3, rubberbandImage);
         // rubberband.render();
+
+        setInterval(gameloop, 120);
     })()
     
 // ========== KEYBOARD INTERACTION LOGIC ============= //
@@ -69,17 +93,9 @@ function keyboardMovement(e){
             toby.render(toby.image, toby.x, toby.y);
             break
         }
-        console.log(toby);
+        // console.log(toby);
         }
         
-    // switch (e.key){
-    //     case "ArrowLeft":
-    //         toby.x > -50 ? toby.x -= 20 : null;
-    //         break
-    //     case "ArrowRight":
-    //         toby.x < (game.width - toby.width) ? toby.x += 20 : null;
-    // }
-
 //---- Event listener to get Toby to move base on keydown ----//
 document.addEventListener('keydown', keyboardMovement);
 
@@ -87,39 +103,34 @@ document.addEventListener('keydown', keyboardMovement);
 function gameloop(){
     ctx.clearRect(toby.x, toby.y, toby.width, toby.height);
 
-//     if(hamburger.alive){
-//         hamburger = new CharacterMaker(0, 0, 50, 50, 3, hamburgerImage);
-//         hamburger.render();
-//         let hit = detectHit(toby, hamburger);
-//     }
-//     toby.render(toby.x, toby.y, toby.width, toby.height);
-}
+    toby.render(toby.x, toby.y, toby.width, toby.height);
+    }
 
-// //--- Detect Hit Logic ---//
-// function detectHit(p1, p2){
-//     let hitTest = 
-//         p1.y + p1.height > p2.y &&
-//         p1.y < p2.y + p2.height &&
-//         p1.x + p1.width > p2.x &&
-//         p1.x < p2.x + p2.width; // {boolean} : if all are true -> hit
+//--- Detect Hit Logic ---//
+function detectHit(p1, p2){
+    let hitTest = 
+        p1.y + p1.height > p2.y &&
+        p1.y < p2.y + p2.height &&
+        p1.x + p1.width > p2.x &&
+        p1.x < p2.x + p2.width; // {boolean} : if all are true -> hit
 
-//     if (hitTest){
-//         let gameScore = Number(score.textContent); //comes in as a string - put number in front - makes it a number
-//         let newScore = gameScore + 100;
-//         score.textContent = newScore;
-//         return addNewItem();
-//     } else {
-//         return false;
-//     }
-// };
+    if (hitTest){
+        let gameScore = Number(score.textContent); //comes in as a string - put number in front - makes it a number
+        let newScore = gameScore + 100;
+        score.textContent = newScore;
+        return addNewItem();
+    } else {
+        return false;
+    }
+};
 
 // // ==== Add new item onto the board randomly falling from top of page ========//
-// function addNewItem(){
-//     hamburger.alive = false;
-//     setTimeout(function(){
-//         let x = Math.floor(Math.random() * game.width) + 10;
-//         let y = 0;
-//         hamburger = new CharacterMaker(x, y, 50, 50, 3, hamburgerImage);
-//     }, 1000)
-//     return true;
-// }
+function addNewItem(){
+    hamburger = new FallingObject(x, y, 50, 50, 3, hamburgerImage);
+    hamburger.alive = false;
+    setTimeout(function(){
+        let x = Math.floor(Math.random() * game.width) + 10;
+        let y = 0;
+    }, 1000)
+    return true;
+}
