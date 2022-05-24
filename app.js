08dc3a368bdd14eefca58e8a6e90b1b1e2f3adf6
tmsheipline <taylorsheipline_2012@hotmail.com>
2022-05-24 10:12:51 -0400

    Changed constructor to function to attempt to get toby on the board

--- app.js
+++ app.js
@@ -7,25 +7,54 @@ canvas.setAttribute("width", getComputedStyle(canvas)["width"])
 canvas.setAttribute("height", getComputedStyle(canvas)["height"])
 
 // ====================== Variables / DOM Manipulation ==================//
-let toby;
+let tobyImage = new Image();
+tobyImage.src = "./img/front view toby.png"
+// console.log(tobyImage);
+// tobyImage.onload = () => {
+//     ctx.drawImage(tobyImage, 50, 50);
+// }
+console.log(tobyImage)
+
 let hamburger;
 let rubberband;
 
-// =====================Class and Constructor for the main character (Toby)===================//
-class Toby {
-    constructor(url, x, y, width, height){
-        this.url = url;
-        this.x = x;
-        this.y = y;
-        this.height = height;
-        this.width = width;
-        this.alive = true;
-        this.render = function() {
-            ctx.drawImage(this.url, this.x, this.y, this.width, this.height);
-        };
-    }
+// =================== Function or class for the main character (Toby)===================//
+function TobyMaker(url, x, y, width, height){
+    this.url = url;
+    this.x = x;
+    this.y = y;
+    this.height = height;
+    this.width = width;
+    this.alive = true;
+    this.render = function (){
+        ctx.drawImage(this.url, this.x, this.y, this.width, this.height);
+    };
 }
+let toby = new TobyMaker("./img/front view toby.png", 200, 400, 20, 40);
+console.log(toby);
+
+
+
+
 // === Event Listener to get Toby on the gameboard ==========//
 window.addEventListener('DOMContentLoaded', function (e){
-
+    toby = new ObjectMaker(tobyImage, 100, 20, 40, 50);
 })
+
+
+// ========== KEYBOARD INTERACTION LOGIC ============= //
+function keyboardMovement(e){
+    console.log('the key that was presssed was' + e.key);
+
+    switch (e.key){
+        case "ArrowLeft":
+            toby.x > 0 ? toby.x -= 10 : null;
+            break
+        case "ArrowRight":
+            toby.x < (game.width - toby.width) ? toby.x += 10 : null;
+
+    }
+// console.log(toby);
+
+
+}
